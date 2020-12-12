a170e09d5626623cb2cd26c67c4f3b3c0bb6c664
Melvyn Koh <melvynkoh94@gmail.com>
2020-12-12 10:24:38 +0800

    Basic Kotlin springboot package

---
+++ src/main/kotlin/com/assessment/KotlinSpringBoot/KotlinSpringBootApplication.kt
@@ -0,0 +1,12 @@
+package com.assessment.KotlinSpringBoot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootApplication>(*args)
+	println("Hello Melvyn")
+}
